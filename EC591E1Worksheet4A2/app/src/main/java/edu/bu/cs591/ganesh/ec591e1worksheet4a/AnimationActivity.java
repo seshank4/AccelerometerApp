2462d088b4331135fd22d16ed7038ef3f7f87c0f
Karunesh Mahajan <Karunesh Mahajan>
2016-10-01 19:11:42 -0400

    Animation Activity changes

--- EC591E1Worksheet4A2/app/src/main/java/edu/bu/cs591/ganesh/ec591e1worksheet4a/AnimationActivity.java
+++ EC591E1Worksheet4A2/app/src/main/java/edu/bu/cs591/ganesh/ec591e1worksheet4a/AnimationActivity.java
@@ -1,13 +1,97 @@
 package edu.bu.cs591.ganesh.ec591e1worksheet4a;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.GestureDetector;
+import android.support.v4.view.GestureDetectorCompat;
+import android.widget.LinearLayout;
 
-public class AnimationActivity extends AppCompatActivity {
+public class AnimationActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    private LinearLayout imgLinearLayout;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+    private SensorManager sManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+        imgLinearLayout = (LinearLayout) findViewById(R.id.imgLinearLayout);
+
+        sManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor accelerometer = sManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+        sManager.registerListener(mSensorListener, sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        //sManager.registerListener((SensorEventListener) this, accelerometer, sManager.SENSOR_DELAY_GAME);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+    }
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sManager.registerListener(mSensorListener, sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        sManager.unregisterListener(mSensorListener);
+        super.onPause();
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
