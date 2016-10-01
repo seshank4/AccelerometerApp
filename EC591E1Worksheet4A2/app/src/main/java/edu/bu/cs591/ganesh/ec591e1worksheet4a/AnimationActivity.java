a1f6f346ad014cb23f140f240b3da4f722d61e72
seshank4 <seshank4@gmail.com>
2016-10-01 14:41:29 -0400

    On flig navigate changes

---
+++ EC591E1Worksheet4A2/app/src/main/java/edu/bu/cs591/ganesh/ec591e1worksheet4a/AnimationActivity.java
@@ -0,0 +1,13 @@
+package edu.bu.cs591.ganesh.ec591e1worksheet4a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+    }
+}
--- EC591E1Worksheet4A2/app/src/main/java/edu/bu/cs591/ganesh/ec591e1worksheet4a/MainActivity.java
+++ EC591E1Worksheet4A2/app/src/main/java/edu/bu/cs591/ganesh/ec591e1worksheet4a/MainActivity.java
@@ -1,13 +1,78 @@
 package edu.bu.cs591.ganesh.ec591e1worksheet4a;
 
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener{
+
+
+    private GestureDetectorCompat detector;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        detector = new GestureDetectorCompat(this, this);
+        detector.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Intent intent = new Intent(getApplicationContext(), AnimationActivity.class );
+        startActivity(intent);
+        return true;
     }
 }
