fd3fcc01e5a3d5bcff084fa18c082de94d94862b
Karunesh Mahajan <Karunesh Mahajan>
2016-10-03 19:32:53 -0400

    shake

--- EC591E1Worksheet4A2/app/src/main/java/edu/bu/cs591/ganesh/ec591e1worksheet4a/AnimationActivity.java
+++ EC591E1Worksheet4A2/app/src/main/java/edu/bu/cs591/ganesh/ec591e1worksheet4a/AnimationActivity.java
@@ -7,6 +7,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.GestureDetector;
 import android.support.v4.view.GestureDetectorCompat;
@@ -14,22 +15,41 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class AnimationActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     private LinearLayout imgLinearLayout;
-    private float mAccel;
-    private float mAccelCurrent;
-    private float mAccelLast;
     private SensorManager sManager;
     private GestureDetectorCompat detector;
     private Animation animationRotateRightFast;
     private Animation animationRotateRightSlow;
     private Animation animationRotateLeftFast;
     private Animation animationRotateLeftSlow;
+    private Animation animationSlideRight;
+    private Animation animationSlideLeft;
+    private Animation animationSlideUp;
+    private Animation animationSlideDown;
+    private Animation animationShake;
     private static final int THRESHOLD_VELOCITY = 10000;
     private static final int MIN_FLING_VELOCITY = 100;
     private ImageView imageView;
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private float deltaX,deltaY,deltaZ;
+    private static final int SIGNIFICANT_SHAKE = 15;
+    private static final int MIN_ACC = 5;
+
+    private float[] gravity = new float[3];
+
+    private float mAccel; // acceleration apart from gravity
+    private float mAccelCurrent; // current acceleration including gravity
+    private float mAccelLast; // last acceleration including gravity
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -45,30 +65,94 @@ public class AnimationActivity extends AppCompatActivity implements GestureDetec
         animationRotateRightSlow = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_right_slow);
         animationRotateLeftFast = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_left_fast);
         animationRotateLeftSlow = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_left_slow);
+        animationSlideRight = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_right);
+        animationSlideLeft = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_left);
+        animationSlideUp = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up);
+        animationSlideDown = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_down);
+        animationShake = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.shake);
         imageView = (ImageView)findViewById(R.id.flingMe);
 
         setContentView(R.layout.activity_animation);
         imgLinearLayout = (LinearLayout) findViewById(R.id.imgLinearLayout);
 
-        sManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        Sensor accelerometer = sManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
-        sManager.registerListener(mSensorListener, sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-        //sManager.registerListener((SensorEventListener) this, accelerometer, sManager.SENSOR_DELAY_GAME);
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(mSensorListener, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        // initialize acceleration values
         mAccel = 0.00f;
         mAccelCurrent = SensorManager.GRAVITY_EARTH;
         mAccelLast = SensorManager.GRAVITY_EARTH;
+
     }
 
     private final SensorEventListener mSensorListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-            mAccelLast = mAccelCurrent;
-            mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
-            float delta = mAccelCurrent - mAccelLast;
-            mAccel = mAccel * 0.9f + delta;
+
+            Sensor mySensor = event.sensor;
+            if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+
+                final float alpha = 0.9f;
+
+                // Isolate the force of gravity with the low-pass filter.
+                gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+                gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+                gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+                float linearX = x - gravity[0];
+                float linearY = y - gravity[1];
+                float linearZ = z - gravity[2];
+
+                deltaX = linearX - last_x;
+                deltaY = linearX - last_y;
+                deltaZ = linearX - last_z;
+
+                long curTime = System.currentTimeMillis();
+
+                mAccelLast = mAccelCurrent;
+                mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
+                float deltaAccl = mAccelCurrent - mAccelLast;
+                mAccel = mAccel * 0.9f + deltaAccl;
+                double deltaTime = curTime - lastUpdate;
+                if ((deltaTime) > 100) {
+                    imageView = (ImageView)findViewById(R.id.flingMe);
+                    if(Math.abs(linearX) > Math.abs((linearY+9.8)) ) {
+                        if (linearX > 0 && Math.abs(linearX) > 5 && deltaX>5 && mAccelCurrent<15) {
+                            imageView.startAnimation(animationSlideLeft);
+                            lastUpdate = curTime;
+                            Log.i("deltas ",String.valueOf(mAccel));
+                            Log.i("acc","mAcc : "+mAccelCurrent);
+                        } else if (linearX < 0 && Math.abs(linearX) > 5 && deltaX<-5 && mAccelCurrent<15) {
+                            imageView.startAnimation(animationSlideRight);
+                            lastUpdate = curTime;
+                            Log.i("deltas ",String.valueOf(mAccel));
+                            Log.i("acc","mAcc : "+mAccelCurrent);
+                        }
+                    }else if(Math.abs(linearY) > Math.abs((linearX))) {
+                        if (linearY >0 && Math.abs(linearY) > 5 && deltaY<-5 && mAccelCurrent>15) {
+                            imageView.startAnimation(animationSlideDown);
+                            lastUpdate = curTime;
+                            Log.i("acc","mAcc : " +mAccelCurrent);
+                        } else if (linearY < 0 && Math.abs(linearY) > 5 && deltaY>2 && mAccelCurrent>5) {
+                            imageView.startAnimation(animationSlideUp);
+                            Log.i("acc","mAcc : "+mAccelCurrent);
+                            lastUpdate = curTime;
+                        }
+                    }
+                    last_x = linearX;
+                    last_y = linearY;
+                    last_z = linearZ;
+
+                    if(mAccel > SIGNIFICANT_SHAKE){
+                        imageView.startAnimation(animationShake);
+                        Log.i("deltas ",String.valueOf(mAccel));
+                    }
+                    }
+                }
         }
 
         @Override
@@ -80,12 +164,12 @@ public class AnimationActivity extends AppCompatActivity implements GestureDetec
     @Override
     protected void onResume() {
         super.onResume();
-        sManager.registerListener(mSensorListener, sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(mSensorListener, senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
-        sManager.unregisterListener(mSensorListener);
+        senSensorManager.unregisterListener(mSensorListener);
         super.onPause();
     }
 
